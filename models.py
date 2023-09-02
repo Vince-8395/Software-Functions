a44da158f80013404669d06de4af96c3e59d7ef0
Vince-8395 <115220055+Vince-8395@users.noreply.github.com>
2023-09-02 20:31:30 +0800

    Add files via upload

--- models.py
+++ models.py
@@ -1,25 +1,30 @@
 from django.db import models
 
-class Professor(models.model):
-    name = models.username(max_length=100)
-    email = models.useremail(maxelength=100)
 
-    def __str__(self):
-        return self.name
+class Professors(models.Model):
+      Professor_FirstName = models.Charfield(max_length=30) 
+      Professor_LastName = models.Charfield(max_length=30)               
+      Professor_EmailAddress = models.Charfield(max_length=30)
 
-class Authors(models.model):
-    name = models.username(max_length=100)
-    email = models.useremail(maxelength=100)
+        def __str__(self):
+            return self.Professors
 
-    def __str__(self):
-        return self.name
-    
-class ResearchTitles(models.model):
-    title = models.ResearchTitle(max_length=100)
-    contents = models.TextField()
-    publish_date = models.publishingdate(blank=True, null=True)
-    Authors = models.AuthorsName(Authors, related_name="posts")
-    CoAuthors = models.CoAuthorsName(CoAuthors, related_name="posts")
-    
-    def __str__(self):
-        return self.researchtitles
+class Titles(models.Model):
+        authors = models.ForeignKey(Professors, on_delete = models.CASCADE)
+        Coauthors = models.ForeignKey(Professors, Students, on_delete = models.CASCADE)
+        name = models.CharField(max_length=100)
+        released_date = models.DateField()
+
+               def __str__(self):
+                    return self.titles
+
+         
+            
+
+
+
+
+
+
+        
+
